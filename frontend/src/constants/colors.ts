9b94abc38926b97abf829948fa96fcb0eed8463d
keihachimaru <keihachimaru@gmail.com>
2026-01-01 09:33:56 +0100

    sources: colors, aimodels, and their logos

---
+++ frontend/src/constants/colors.ts
@@ -0,0 +1,6 @@
+export const colors = [
+    '#4285F4',
+    '#EA4335',
+    '#FBBC04',
+    '#34A853',
+]
---
+++ frontend/src/constants/models.ts
@@ -0,0 +1,18 @@
+import deepseek from '../assets/deepseek.png';
+import gemini from '../assets/gemini.png';
+
+export const aiModels = ["deepseek", "gemini"];
+export const modelDetails = {
+    "deepseek" : {
+        name: "deepseek",
+        color: '#4D6BFE',
+        logo: deepseek,
+        key: null,
+    },
+    "gemini" : {
+        name: "gemini",
+        color: '#9177C7', 
+        logo: gemini,
+        key: null,
+    },
+}
