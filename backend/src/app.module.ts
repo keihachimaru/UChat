9ef9c331889174c679c7ddd623b27abe1ec404f1
keihachimaru <keihachimaru@gmail.com>
2026-01-15 16:14:32 +0100

    feat: document module initalization

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,12 +5,15 @@ import { MongoDBProvider } from './database/mongodb';
 
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
+import { DocumentModule } from './document/document.module';
+import { DocumentModule } from './document/document.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     UsersModule, AuthModule,
-    MongoDBProvider
+    MongoDBProvider,
+    DocumentModule
   ],
 })
 
