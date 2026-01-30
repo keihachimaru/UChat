2ade9369fc648ee841ba96c80463bd14ec8b4128
keihachimaru <keihachimaru@gmail.com>
2026-01-30 01:26:27 +0100

    bug: try to fix cors error

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,12 +5,13 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser());  
-
   app.enableCors({
     origin: ['http://localhost:5173', 'https://u-chat-theta.vercel.app'],
     credentials: true,
   })
+
+  app.use(cookieParser());  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
